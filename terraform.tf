929efe6167cca0892265a109c0858926d61ebe96
Chandra Kumar <ckbitra@gmail.com>
2026-02-10 12:45:17 -0500

    remove the ln profile

--- terraform.tf
+++ terraform.tf
@@ -10,6 +10,5 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
