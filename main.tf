0ce227c6372694b66a3ee43a239e51487d2def11
Chandra Kumar <ckbitra@gmail.com>
2026-02-10 12:19:04 -0500

    Add ec2

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
-resource "random_string" "name" {
-    length = 16
+resource "aws_instance" "example" {
+  ami           = "ami-0c1fe732b5494dc14"  # Your AMI ID (e.g., Amazon Linux 2023)
+  instance_type = "t3.micro"
+
+  subnet_id = "subnet-065963ad393ab557b"  # Existing subnet ID
+
+  tags = {
+    Name        = "MyEC2Instance"
+    Environment = "dev"
+    Owner       = "Bitra Team"
+  }
 }
