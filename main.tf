5509e8ae72e46695c65421f7bb12dae5486f1a0d
Chandra Kumar <ckbitra@gmail.com>
2026-02-11 09:11:20 -0500

    update code

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "this" {
   ami           = "ami-0c1fe732b5494dc14" # Your AMI ID (e.g., Amazon Linux 2023)
   instance_type = "t3.micro"
 
-  subnet_id = "subnet-07b2351cc4577c5ac" # Existing subnet ID
+  subnet_id = "subnet-0878051ef7c33ddff" # Existing subnet ID
 
   tags = {
     Name        = "MyEC2Instance"
@@ -17,7 +17,7 @@ resource "aws_instance" "this" {
 resource "aws_security_group" "my-new-security-group" {
   name        = "web_server_inbound"
   description = "Allow inbound traffic on tcp/443"
-  vpc_id      = "vpc-0e208acff9cea002c"
+  vpc_id      = "vpc-043e913b79518510c"
 
   ingress {
     description = "Allow 443 from the Internet"
